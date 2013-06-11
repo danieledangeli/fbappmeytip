e786168c2d6bb03265cd801de7b8250c038f4b16
erlangb <dangeli88.daniele@gmail.com>
2013-06-11 11:59:39 +0200

    fix login

--- fbapp.php
+++ fbapp.php
@@ -87,17 +87,17 @@ include('php/header.php');
 
 
     <?php } else { ?>
-    <div>
+    <!-- <div>
         <h1>Please login in</h1>
         <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
-    </div>
+    </div> -->
     <?php } ?>
 </header>
 
-<?php if(isset($basic)) { ?>
+<?php if(!isset($basic)) { ?>
 
 <div id="myModal" class="reveal-modal small">
-    <span class="success label">Success!</span><br>
+    <span class="success label">Ok</span><br>
     <span class="radius secondary label"><?php echo $meytipuser->name;?></span>
     <span class="radius secondary label">La tua scommessa è stata registrata con successo!</span>
 
@@ -105,88 +105,7 @@ include('php/header.php');
     <a class="close-reveal-modal">&#215;</a>
 </div>
 
-<!-- HEADER -->
-<nav class="top-bar">
-    <ul class="title-area">
-        <li class="name logo"><a href="fbapp.php"><img src="img/logo.png"></a></li>
-        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-    </ul>
-    <section class="top-bar-section">
-        <ul class="left">
-            <li class="divider"></li>
-            <li class="active"><a href="#">events</a></li>
-            <li class="divider"></li>
-            <li><a href="#">people</a></li>
-            <li class="divider"></li>
-        </ul>
-    </section>
-</nav>
-<!-- /HEADER -->
-<!--USER PANEL -->
 
-
-<div class="userpanel">
-    <div class="row">
-        <!-- NOME -->
-        <div class="large-3 columns verticaldivider">
-            <div class="large-5 small-6 columns">
-                <img src="https://graph.facebook.com/<?php echo $user_id; ?>/picture?type=square" alt="<?php echo $basic['name'];?>">
-            </div>
-            <div class="large-7 small-6 collapsecolumns">
-                <h4 class="white"><?php echo $basic['name']; ?></h4>
-                <div><h8>Pending Bets</h8> <div class="valuesmall" id="pending"><?php echo $meytipuser->pending; ?></div></div>
-                <div><h8>Challenge</h8> <div class="valuesmall">0</div></div>
-            </div>
-        </div>
-        <!-- /NOME -->
-        <!-- SALVADANAIO -->
-        <div class="large-2 columns verticaldivider">
-            <div class="row">
-                <div class="large-5 small-6 columns"><i><img src="img/porco.png" alt=""></i></div>
-                <div class="large-7 small-6 collapsecolumns">
-                    <h4>Salvadanaio</h4>
-                    <div class="value" id="cash"><?php echo $meytipuser->cash; ?></div>
-                </div>
-            </div>
-        </div>
-        <!-- /SALVADANAIO -->
-        <!-- BEST CASE -->
-        <div class="large-2 columns verticaldivider">
-            <div class="row">
-                <div class="large-5 small-6 columns"><i><img src="img/win.png" alt=""></i></div>
-                <div class="large-7 small-6 collapsecolumns">
-                    <h4>Best Case</h4>
-                    <div class="value">0</div>
-                </div>
-            </div>
-        </div>
-        <!-- /BEST CASE -->
-        <!-- WORST CASE -->
-        <div class="large-2 columns verticaldivider">
-            <div class="row">
-                <div class="large-5 small-6 columns"><i><img src="img/lose.png" alt=""></i></div>
-                <div class="large-7 small-6 collapsecolumns">
-                    <h4>Worst Case</h4>
-                    <div class="value">0</div>
-                </div>
-            </div>
-        </div>
-        <!-- /WORST CASE -->
-        <!-- COIN -->
-        <div class="large-3 columns">
-            <div class="large-6 small-6 columns left">
-                <h4>Daily Coins</h4>
-                <a class="tiny button expand" href="#">Ritira</a>
-            </div>
-            <div class="large-6 small-6 columns">
-                <h4>Extra Coins</h4>
-                <a class="tiny success button expand" href="#">Ritira</a>
-            </div>
-        </div>
-        <!--/COIN-->
-    </div>
-</div>
-<!--/USER PANEL -->
 
 
 <div class="row centrale">
--- php/header.php
+++ php/header.php
@@ -98,7 +98,7 @@ function CallAPI($method, $url, $data = false)
     // Optional Authentication:
     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
@@ -106,4 +106,86 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 }
 
 ?>
+<!-- HEADER -->
+<nav class="top-bar">
+    <ul class="title-area">
+        <li class="name logo"><a href="fbapp.php"><img src="img/logo.png"></a></li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+    </ul>
+    <section class="top-bar-section">
+        <ul class="left">
+            <li class="divider"></li>
+            <li class="active"><a href="#">events</a></li>
+            <li class="divider"></li>
+            <li><a href="#">people</a></li>
+            <li class="divider"></li>
+        </ul>
+    </section>
+</nav>
+<!-- /HEADER -->
+<!--USER PANEL -->
+
+
+<div class="userpanel">
+    <div class="row">
+        <!-- NOME -->
+        <div class="large-3 columns verticaldivider">
+            <div class="large-5 small-6 columns">
+                <img src="https://graph.facebook.com/<?php echo $user_id; ?>/picture?type=square" alt="<?php echo $basic['name'];?>">
+            </div>
+            <div class="large-7 small-6 collapsecolumns">
+                <h4 class="white"><?php echo $basic['name']; ?></h4>
+                <div><h8>Pending Bets</h8> <div class="valuesmall" id="pending"><?php echo $meytipuser->pending; ?></div></div>
+                <div><h8>Challenge</h8> <div class="valuesmall">0</div></div>
+            </div>
+        </div>
+        <!-- /NOME -->
+        <!-- SALVADANAIO -->
+        <div class="large-2 columns verticaldivider">
+            <div class="row">
+                <div class="large-5 small-6 columns"><i><img src="img/porco.png" alt=""></i></div>
+                <div class="large-7 small-6 collapsecolumns">
+                    <h4>Salvadanaio</h4>
+                    <div class="value" id="cash"><?php echo $meytipuser->cash; ?></div>
+                </div>
+            </div>
+        </div>
+        <!-- /SALVADANAIO -->
+        <!-- BEST CASE -->
+        <div class="large-2 columns verticaldivider">
+            <div class="row">
+                <div class="large-5 small-6 columns"><i><img src="img/win.png" alt=""></i></div>
+                <div class="large-7 small-6 collapsecolumns">
+                    <h4>Best Case</h4>
+                    <div class="value">0</div>
+                </div>
+            </div>
+        </div>
+        <!-- /BEST CASE -->
+        <!-- WORST CASE -->
+        <div class="large-2 columns verticaldivider">
+            <div class="row">
+                <div class="large-5 small-6 columns"><i><img src="img/lose.png" alt=""></i></div>
+                <div class="large-7 small-6 collapsecolumns">
+                    <h4>Worst Case</h4>
+                    <div class="value">0</div>
+                </div>
+            </div>
+        </div>
+        <!-- /WORST CASE -->
+        <!-- COIN -->
+        <div class="large-3 columns">
+            <div class="large-6 small-6 columns left">
+                <h4>Daily Coins</h4>
+                <a class="tiny button expand disabled" href="#">Ritira</a>
+            </div>
+            <div class="large-6 small-6 columns">
+                <h4>Extra Coins</h4>
+                <a class="tiny success button expand disabled" href="#">Ritira</a>
+            </div>
+        </div>
+        <!--/COIN-->
+    </div>
+</div>
+<!--/USER PANEL -->
 
