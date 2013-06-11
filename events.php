652646af411777b16efb8072b04d614491a16409
erlangb <dangeli88.daniele@gmail.com>
2013-06-11 12:45:46 +0200

    header clear

--- events.php
+++ events.php
@@ -19,18 +19,6 @@ include('php/header.php');
 </head>
 <body>
 
-<?php if (isset($basic)) {
-    $data = json_encode($basic);
-    $us = CallAPI('POST','https://meytip.com/back/meytip/web/app.php/login.json',$data);
-    $meytipuser = json_decode($us);
-    ?>
-<?php } else { ?>
-    <body>
-    <div class = "row">
-        <div class="fb-login-button" size="xlarge" length="xlarge" data-scope="publish_stream,user_likes,user_photos"></div>
-    </div>
-    </body>
-<?php } ?>
 <?php
 include('php/userpanel.php');
 ?>
@@ -75,6 +63,18 @@ include('php/userpanel.php');
 </script>
 <header class="clearfix">
 
+    <?php if (isset($basic)) {
+        $data = json_encode($basic);
+        $us = CallAPI('POST','https://meytip.com/back/meytip/web/app.php/login.json',$data);
+        $meytipuser = json_decode($us);
+	?>
+  <?php } else { ?>
+        <body>
+        <div class = "row">
+            <div class="fb-login-button" size="xlarge" length="xlarge" data-scope="publish_stream,user_likes,user_photos"></div>
+        </div>
+        </body>
+    <?php } ?>
 </header>
 
 <?php if(isset($basic)){ ?>
