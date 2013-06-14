51171967112f2dbaeb3dbb29eeb675b43ca7d681
daniele <daniele@daniele-Latitude-E6510.(none)>
2013-06-14 12:50:39 +0200

    die

--- php/header.php
+++ php/header.php
@@ -44,6 +44,7 @@ if ($user_id) {
     } catch (FacebookApiException $e) {
 // If the call fails we check if we still have a user. The user will be
 // cleared if the error is because of an invalid accesstoken
+        die('here');
         if (!$facebook->getUser()) {
             header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
             exit();
