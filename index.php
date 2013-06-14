60eade24b14e264854fc7c212b3234df4c645b93
daniele <daniele@daniele-Latitude-E6510.(none)>
2013-06-14 14:33:18 +0200

    redirect

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include('php/header.php');
         window.fbAsyncInit = function() {
             FB.init({
                 appId      : '<?php echo AppInfo::appID(); ?>', // App ID
-                channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
+
                 status     : true, // check login status
                 cookie     : true, // enable cookies to allow the server to access the session
                 xfbml      : true // parse XFBML
@@ -27,13 +27,13 @@ include('php/header.php');
             // Listen to the auth.login which will be called when the user logs in
             // using the Login button
             FB.Event.subscribe('auth.login', function(response) {
-                 window.location.href='https://meytip.com/fbappmeytip/index.php';
+                 //window.location.href='https://meytip.com/fbappmeytip/index.php';
                 // We want to reload the page now so PHP can read the cookie that the
                 // Javascript SDK sat. But we don't want to use
                 // window.location.reload() because if this is in a canvas there was a
                 // post made to this page and a reload will trigger a message to the
                 // user asking if they want to send data again.
-               window.location = window.location;
+               window.location = 'https://meytip.com/fbappmeytip/index.php';
             });
 
             FB.Canvas.setAutoGrow();
