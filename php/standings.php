c4fb195b6df8e9d9ad1f20aff948bddb3472f49c
erlangb <dangeli88.daniele@gmail.com>
2013-06-22 16:35:14 +0200

    standts

--- php/standings.php
+++ php/standings.php
@@ -1,8 +1,9 @@
 <?php if (isset($basic)) {
 
-    $users = CallAPI('POST','https://meytip.com/back/meytip/web/app.php/stands/innlab.json');
+    $users = CallAPI('GET','https://meytip.com/back/meytip/web/app.php/stands/innlab.json');
+
     $users = json_decode($users);
-    die(var_dump($users));
+    
 
 
 } else { ?>
