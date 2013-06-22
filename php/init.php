ed5128faff4a3072f1cfced5fc0f71f9840ed821
erlangb <dangeli88.daniele@gmail.com>
2013-06-22 15:47:12 +0200

    windows.location.reload

--- php/init.php
+++ php/init.php
@@ -5,6 +5,7 @@
 
     $feed = CallAPI('GET','https://meytip.com/back/meytip/web/app.php/feeds/10.json');
     $feed = json_decode($feed);
+
     $userdata = CallAPI('GET','https://meytip.com/back/meytip/web/app.php/users/'.$meytipuser->facebookid.'/feeds.json');
     $userfeed = json_decode($userdata);
 
